fc1b686cf10fa61c2c528e359712f608685398d4
lyan <lyan@ubuntu.(none)>
2012-10-22 11:35:37 -0700

    proof by johnathan

--- CODEFORCE/prob236C.cpp
+++ CODEFORCE/prob236C.cpp
@@ -1,18 +1,18 @@
 // =========================================================
-// 
+//
 //       Filename:  prob236C.cpp
-// 
-//    Description:  
-// 
+//
+//    Description:
+//
 //        Version:  1.0
 //        Created:  10/21/2012 06:25:38 PM
 //       Revision:  none
 //       Compiler:  g++
-// 
+//
 //         Author:  LI YAN (lyan), lyan@cs.ucr.edu
 //        Company:  U of California Riverside
 //      Copyright:  Copyright (c) 10/21/2012, LI YAN
-// 
+//
 // =========================================================
 
 #include <algorithm>
@@ -47,4 +47,14 @@ int main()
 
 // Accepted
 // but how do you prove it?
+//
+// for n odd, it must be n,(n-1),(n-2)
+// for n even, one option is to use (n-1), (n-2), (n-3)
+// the only other options that have to beat the above are
+// the first one has to be n, or it will be dominated by the above
+// the second one is then either n-1 or n-2, using n-3 would result in
+// n, n-3, n-4 and the product is already too small
+// n, n-1, n-a for some a such that n(n-1)(n-a) >= (n-1)(n-2)(n-3),
+// then a cannot be too small
+// same thing n, n-2, n-b, and b cannot be too small
 //

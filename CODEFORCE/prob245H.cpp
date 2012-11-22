516311f3a1c8f64341b14e885878f5e7d3e3990a
Li Yan <lyan@lyan-ThinkPad-T430.(none)>
2012-11-22 11:28:45 -0800

    done with prob 245

--- CODEFORCE/prob245H.cpp
+++ CODEFORCE/prob245H.cpp
@@ -1,69 +1,46 @@
-// O(n^2) algorithm to find all substr that is palindrom
+// This is a DP problem
+// dp[i+1][j+1] = 1 if s[i..j] is a pal
+// let a[i][j] be the number of pal substr in s[i..j], 1-based
+// a[][] can be computed in O(n^2) time
+// then query can be answered by a simple lookup
 
-#include <algorithm>
-#include <cassert>
-#include <iostream>
-#include <string>
-#include <utility>
+#include <cstdio>
+#include <cstring>
 using namespace std;
 
-typedef pair<int, int> pii;
-
-pii interval[10000+10];
-
-int bsearch(int n, pii seg)  // seg is not in 
-{
-  int l, r;
-  l=-1, r=n;  // interval[l] < seg <= interval[r]
-  while (l+1 < r) {
-    int m = (l+r)/2;  // l < m < r, and m is valid
-    if (interval[m] < seg) l = m;
-    else r = m;
-  }
-  assert(l+1 == r);
-  return r;
-}
+char s[5005];
+int dp[5005][5005];
+int a[5005][5005];
 
 int main()
 {
-  string s;
-  cin >> s;
-  int n = s.length();
-  int l, r;
-  int cnt = 0;
-  for (int i=1; i<2*n; ++i) {
-    // even pos, i/2-1 and i/2
-    // odd pos, i/2
-    if (i&1) {
-      for (l=r=i/2; 0<=l && r<n && s[l] == s[r]; --l, ++r) ;
-      interval[cnt++] = pii(l+1, r-1);
-    } else {
-      for (l=i/2-1, r=i/2; 0<=l && r<n && s[l] == s[r]; --l, ++r) ;
-      if (l+1 < r-1) interval[cnt++] = pii(l+1, r-1);
-    }
-  }
+    scanf("%s", s);
+    int n = strlen(s);
+    for (int i=0; i<n; ++i)
+        dp[i+1][i+1] = 1;
+    for (int i=0; i+1<n; ++i)
+        if (s[i] == s[i+1])
+            dp[i+1][i+2] = 1;
+    for (int len=3; len<=n; ++len)
+        for (int i=0; i+len <=n; ++i) {
+            int j = i+len-1;
+            if (s[i] == s[j] && dp[i+1+1][j-1+1])
+                dp[i+1][j+1] = 1;
+        }
 
-  int q; cin >> q;
-  for (int i=0; i<q; ++i) {
-    int ans = 0;
-    int a, b; cin >> a >> b; --a, --b;
-    // find number of intervals contained in [a,b]
-    for (int i=0; i<cnt; ++i) {
-    	int a1, a2, c1, c2;
-	a1 = interval[i].first; a2 = interval[i].second;
-	if (a2-a1+1 & 1) {  // odd pal
-	  c1 = c2 = (a1 + a2) / 2;
-	} else {  // even pal
-	  c1 = (a1 + a2) / 2;
-	  c2 = (a1 + a2) / 2 + 1;
-	}
-	a1 = max(a1, a);
-	a2 = min(a2, b);
-	if (a1 <= c1 && c2 <= a2)
-	  ans += 1 + min(c1-a1, a2-c2);
+    // dp[i+1][j+1] = dp[i+1][j] + dp[i+2][j+1] - dp[i+2][j]
+    for (int len=1; len<=n; ++len) {
+        for (int i=0; i<n; ++i) {
+            int j=i+len-1;
+            a[i+1][j+1] = a[i+1][j] + a[i+2][j+1] - a[i+2][j] + dp[i+1][j+1];
+        }
     }
-    cout << ans << endl;
-  }
+
+    int q; scanf("%d", &q);
+    while (q--) {
+        int l, r; scanf("%d%d", &l, &r);
+        printf("%d\n", a[l][r]);
+    }
 }
 
-// TLE
+// Accepted

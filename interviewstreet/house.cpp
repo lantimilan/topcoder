d3992eaaea649b6af431419114debcc6192d0c51
Li Yan <lantimilan@gmail.com>
2012-11-26 21:08:09 -0800

    house 17/20 passed

--- interviewstreet/house.cpp
+++ interviewstreet/house.cpp
@@ -1,7 +1,9 @@
 /* Enter your code here. Read input from STDIN. Print output to STDOUT */
 
 // the trajectory of a point with d1/d2 = a for (x1, y1) and (x2, y2) is a circle!
+#include <algorithm>
 #include <cmath>
+#include <iomanip>
 #include <iostream>
 using namespace std;
 
@@ -16,34 +18,45 @@ void get_circle(double ratio, double x1, double y1, double x2, double y2,
     xc = (a2*x2 - x1)/(a2-1);
     yc = (a2*y2 - y1)/(a2-1);
     double t1, t2;
-    t1 = sqr(a2*x2-x1) - (a2-1)*(a2*x2*x2-x1*x1);
-    t2 = sqr(a2*y2-y1) - (a2-1)*(a2*y2*y2-y1*y1);
-    radius = sqrt((t1+t2)/sqr(a2-1));
+    t1 = sqr((a2*x2-x1)/(a2-1)) - 1/(a2-1)*(a2*x2*x2-x1*x1);
+    t2 = sqr((a2*y2-y1)/(a2-1)) - 1/(a2-1)*(a2*y2*y2-y1*y1);
+    radius = sqrt(t1+t2);
+    //cout << "radius " << radius << " xc " << xc << " yc " << yc << endl;
 }
 
 // http://2000clicks.com/mathhelp/GeometryConicSectionCircleIntersection.aspx
 // K = (1/4)sqrt(((rA+rB)2-d2)(d2-(rA-rB)2))
 // d2 = (xB-xA)2 + (yB-yA)2 
 // x = (1/2)(xB+xA) + (1/2)(xB-xA)(rA2-rB2)/d2 ± 2(yB-yA)K/d2 
-// y = (1/2)(yB+yA) + (1/2)(yB-yA)(rA2-rB2)/d2 ± -2(xB-xA)K/d2                   
+// y = (1/2)(yB+yA) + (1/2)(yB-yA)(rA2-rB2)/d2 ± -2(xB-xA)K/d2
+// this one has poor precision, instead use (0,0) and (d,0)
+// and vector arithmetic with complex number to derive axis rotation and translation
 void intersect_circle(double r1, double x1, double y1,
                      double r2, double x2, double y2,
                      double &x3, double &y3,
                      double &x4, double &y4)
 {
-    double K, d2, t1, t2, t3, s1, s2, s3;
+    //cout << "r1 x1 y1 r2 x2 y2 " 
+    //    << r1 << ' ' << x1 << ' ' << y1 << ' '
+    //    << r2 << ' ' << x2 << ' ' << y2 << endl;
+    double d, d2, u1, w1, u2, w2, xnew, ynew;
     d2 = sqr(x2-x1) + sqr(y2-y1);
-    K  = 1.0/4 * sqrt((sqr(r1+r2) - d2)*(d2 - sqr(r1-r2)));
-    t1 = 1.0/2*(x1+x2);
-    t2 = 1.0/2 * (x2-x1)*(r1*r1-r2*r2)/d2;
-    t3 = 2*(y2-y1)*K/d2;
-    s1 = 1.0/2*(y1+y2);
-    s2 = 1.0/2 * (y2-y1)*(r1*r1-r2*r2)/d2;
-    s3 = 2*(x2-x1)*K/d2;
-    x3 = t1 + t2 + t3;
-    x4 = t1 + t2 - t3;
-    y3 = s1 + s2 + s3;
-    y4 = s1 + s2 - s3;
+    d = sqrt(d2);
+    xnew = (r1*r1 - r2*r2 + d2)/(2*d);
+    ynew = sqrt(r1*r1 - xnew*xnew);
+    //cout << "xnew, ynew " << xnew << ' ' << ynew << endl;
+    u2 = x2 - x1; w2 = y2 - y1;
+    u1 = 1/d * (u2*xnew - w2*ynew);
+    w1 = 1/d * (w2*xnew + u2*ynew);
+    x3 = x1 + u1;
+    y3 = y1 + w1;
+    ynew = -ynew;
+    u1 = 1/d * (u2*xnew - w2*ynew);
+    w1 = 1/d * (w2*xnew + u2*ynew);
+    x4 = x1 + u1;
+    y4 = y1 + w1;    
+    if (x3 < x4 || x3==x4 && y3 < y4) { x4=x3; y4=y3; }
+    //cout << x3 << ' ' << y3 << ' ' << x4 << ' ' << y4 << endl;
 }
                   
 int main()
@@ -55,10 +68,11 @@ int main()
         cin >> x[i] >> y[i];
     double xc1, yc1, r1, xc2, yc2, r2;
     get_circle(a, x[0], y[0], x[1], y[1], r1, xc1, yc1);
-    get_circle(b, x[2], y[2], x[3], y[3], r2, xc2, yc1);
+    get_circle(b, x[2], y[2], x[3], y[3], r2, xc2, yc2);
     if (r1 < r2) {
         swap(r1, r2); swap(xc1, xc2); swap(yc1, yc2);
     }
+
     // four cases for two circles
     double dist = sqrt(sqr(xc1-xc2) + sqr(yc1-yc2));
     string nosol = "Impossible!";
@@ -68,7 +82,7 @@ int main()
         // one sol
         double x3, y3, x4, y4;
         intersect_circle(r1, xc1, yc1, r2, xc2, yc2, x3, y3, x4, y4);
-        cout << x4 << ' ' << y4 << endl;
+        cout << fixed << setprecision(2) << x4 << ' ' << y4 << endl;
     } else {
         if (r1 == r2 && xc1 == xc2 && yc1 == yc2) {
             // two circles overlap
@@ -81,7 +95,7 @@ int main()
                 // one or two sol
                 double x3, y3, x4, y4;
                 intersect_circle(r1, xc1, yc1, r2, xc2, yc2, x3, y3, x4, y4);
-                cout << x4 << ' ' << y4 << endl;
+                cout << fixed << setprecision(2) << x4 << ' ' << y4 << endl;
             } else {
                 cout << nosol << endl;  // no sol
             }

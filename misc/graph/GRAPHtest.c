a7c114e520b21af84da4c59e1072f03af8208e90
Li Yan <lyan@lyan-ThinkPad-T430.(none)>
2012-12-25 18:48:06 -0800

    update on test program

--- misc/graph/GRAPHtest.c
+++ misc/graph/GRAPHtest.c
@@ -7,8 +7,20 @@
 
 int main(int argc, char *argv[])
 {
-    int V = atoi(argv[1]), E = atoi(argv[2]);
+    int i, v, w;
+    int V, E;
+
+    if (argc != 3) {
+        puts("Usage: a.out <V> <E> < edgelist.txt");
+        exit(1);
+    }
+    V = atoi(argv[1]), E = atoi(argv[2]);
     Graph G = GRAPHinit(V, E);
+    for (i=0; i<E; ++i) {
+        scanf("%d%d", &v, &w);
+        GRAPHinsertE(G, EDGE(v, w));
+    }
+
     if (V < 20)
         GRAPHshow(G);
     else printf("%d vertices, %d edges, ", V, E);

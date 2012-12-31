e53b0fe70e5b2f57489a5ddde436a9a40c37fd76
Li Yan <lyan@lyan-ThinkPad-T430.(none)>
2012-12-30 17:06:57 -0800

    no idea for E

--- CODEFORCE/prob260E.cpp
+++ CODEFORCE/prob260E.cpp
@@ -10,4 +10,45 @@
 // integer or .5 places
 //
 // time: 9! * log(10^9) = 10^6 * 30 = 10^7
+//
+// Question: if row[3] and col[3] are good, does that imply all a[0..8] are good?
 /////////////////////////////////////////////////////////////////////
+
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+const int M = 1e5;
+int X[M+5];
+int Y[M+5];
+int a[10];
+
+int bsearch(int lo, int hi, int target)
+{
+}
+
+int main()
+{
+    int n; cin >> n;
+    int xmin, xmax, ymin, ymax;
+    for (int i=0; i<n; ++i) { cin >> X[i] >> Y[i]; X[i] <<= 1; Y[i] <<= 1; }
+    for (int i=0; i<9; ++i) cin >> a[i];
+
+    xmin = xmax = X[0]; ymin = ymax = Y[0];
+    for (int i=1; i<n; ++i) {
+        xmin = min(xmin, X[i]);
+        xmax = max(xmax, X[i]);
+        ymin = min(ymin, Y[i]);
+        ymax = max(ymax, Y[i]);
+    }
+    xmin--; xmax++; ymin--; ymax++;
+
+    int perm[9];
+    for (int i=0; i<9; ++i) perm[i] = i;
+    do {
+        int row[3] = {0}, col[3] = {0};
+        for (int i=0; i<3; ++i) for (int j=0; j<3; ++j) row[i] += a[perm[i*3+j]];
+        for (int i=0; i<3; ++i) for (int j=0; j<3; ++j) col[i] += a[perm[i+j*3]];
+
+    } while (next_permutation(perm, perm+9));
+}

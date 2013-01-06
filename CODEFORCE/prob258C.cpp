d6616afbfbada6c4c848f8f33dd32666fe3ba032
Li Yan <lantimilan@gmail.com>
2013-01-05 19:17:37 -0800

    CF 258C

--- CODEFORCE/prob258C.cpp
+++ CODEFORCE/prob258C.cpp
@@ -5,5 +5,8 @@
 //
 // for each valid seq, one needs to be lcm, the rest can be
 // any divisor (including lcm itself)
+// this is all k^n minus (k-1)^n, where k is number of divisors of x
 //
 // n is 10^5
+// order of elements is no interest, can sort or count number of occurrence
+// of each val

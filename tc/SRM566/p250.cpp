fcb80d9c789f9917cfc059ff2e4e198b91ccad94
Li Yan <lyan@lyan-ThinkPad-T430.(none)>
2013-01-12 11:29:58 -0800

    adj[][] is the way to write code

--- tc/SRM566/p250.cpp
+++ tc/SRM566/p250.cpp
@@ -7,6 +7,7 @@
 // update: build adj matrix helps find triangle
 // Gassa has almost the same code, why I cannot pass the last sample testcase?
 #include <algorithm>
+#include <cstring>
 #include <iostream>
 #include <set>
 #include <vector>
@@ -28,21 +29,13 @@ long long countDesigns(int n, vector <int> p1, vector <int> p2)
     ans += (1LL<<nb) - (1+nb);
     }
     // triangle
-    int a[3];
-    for (a[0]=1; a[0]<=n; ++a[0])
-    for (a[1]=a[0]+1; a[1]<=n; ++a[1])
-    for (a[2]=a[1]+1; a[2]<=n; ++a[2])
-    {
-        bool edge[3] = {0};
-        for (int x=0; x<3; ++x) {
-            int b1 = a[x], b2 = a[(x+1)%3];
-            for (int i=0; i<m; ++i)
-                if (p1[i] == b1 && p2[i] == b2
-                	|| p1[i] == b2 && p2[i] == b1) edge[x] = 1;
-        }
-        ans += edge[0] && edge[1] && edge[2];
-    }
-
+    int a[60][60]; memset(a, 0, sizeof a);
+    for (int i=0; i<m; ++i) a[p1[i]][p2[i]] = a[p2[i]][p1[i]] = 1;
+    for (int b1=1; b1<=n; ++b1)
+    for (int b2=b1+1; b2<=n; ++b2)
+    for (int b3=b2+1; b3<=n; ++b3)
+        if (a[b1][b2] && a[b2][b3] && a[b3][b1])
+            ans++;
     return ans;
 }
 };

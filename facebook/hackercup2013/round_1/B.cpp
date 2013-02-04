dc62c61a5faa0f66b9fc3301dad3544ad8522d9a
lyan <lyan@lyan-ThinkPad-T430.(none)>
2013-02-04 10:20:03 -0800

    lexico means try all a to f

--- facebook/hackercup2013/round_1/B.cpp
+++ facebook/hackercup2013/round_1/B.cpp
@@ -1,7 +1,11 @@
 // bipartite matching
 // but how to get lexico minimum matching
+//
+// previous solution sorts edge by matched string (breaks when ties)
+// the correct idea is to try every a-f for ?
 
 #include <algorithm>
+#include <cassert>
 #include <cstring>
 #include <iostream>
 #include <string>
@@ -69,13 +73,49 @@ string get_str(const string &s1, const string &s2)
     return ans;
 }
 
-void solve(int tcase)
+bool check(int m, const string &s1, const string &s2)
 {
     memset(mate, -1, sizeof mate);
-    for (int i=0; i<300; ++i)
-        adj[i].clear();
+    for (int i=0; i<300; ++i) adj[i].clear();
+
+    string left[105], right[105];
+    int l = s1.length();
+    int sub = l/m;
+    for (int i=0, j=0; i<l; i+=sub, j++) {
+        left[j] = s1.substr(i, sub);
+        right[j] = s2.substr(i, sub);
+    }
+    for (int i=0; i<m; ++i) {
+        for (int j=m; j<2*m; ++j) {
+            if (match(left[i], right[j-m])) {
+                string s = get_str(left[i], right[j-m]);
+                adj[i].push_back(psi(s,j));
+            }
+        }
+    }
+    for (int i=0; i<m; ++i) {
+        sort(adj[i].begin(), adj[i].end());
+    }
+    int size = bipartite(m);
+    return size == m;
+}
+
+void solve(int tcase)
+{
     int m; cin >> m;
     string s1, s2; cin >> s1 >> s2;
+    if (!check(m, s1, s2)) {
+        cout << "Case #" << tcase << ": ";
+        cout << "IMPOSSIBLE" << endl; return;
+    }
+    for (int i=0; i<s1.length(); ++i) if (s1[i] == '?')
+    {
+    for (s1[i] = 'a'; s1[i] <= 'f'; ++s1[i])
+    {
+
+    memset(mate, -1, sizeof mate);
+    for (int i=0; i<300; ++i) adj[i].clear();
+
     string left[105], right[105];
     int l = s1.length();
     int sub = l/m;
@@ -97,15 +137,13 @@ void solve(int tcase)
         sort(adj[i].begin(), adj[i].end());
     }
     int size = bipartite(m);
-    cout << "Case #" << tcase << ": ";
-    if (size < m) { cout << "IMPOSSIBLE" << endl; return; }
+    if (size == m) break;
+    }
+    assert(s1[i] <= 'f');
+    }
+    cout << "Case #" << tcase << ": " << s1 << endl;;
     // look for lexico smallest match
     //cout << "perfect match" << endl;
-    for (int i=0; i<m; ++i) {
-        int j = mate[i];
-        cout << get_str(left[i], right[j-m]);
-    }
-    cout << endl;
 }
 
 int main()

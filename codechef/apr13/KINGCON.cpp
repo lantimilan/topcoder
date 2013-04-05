5b1051be01cb2846be31b0baf5b60a3dc82ee841
lyan <lyan@lyan-ThinkPad-T430.(none)>
2013-04-05 15:51:07 -0700

    OK, have 5 now

--- codechef/apr13/KINGCON.cpp
+++ codechef/apr13/KINGCON.cpp
@@ -11,15 +11,18 @@
 // i is an articulation point if low[i] = pre[i] for non-root i
 // for root, it is articulation point iff root has more than 1 branching
 
+#include <cstdio>
 #include <cstring>
 #include <iostream>
 using namespace std;
 
+int nedge[3005];
 int adj[3005][3005];
 int parent[3005];
 int pre[3005];
 int low[3005];
 int vis[3005];
+bool is_ap[3005];
 int N, M, K;
 int cnt;
 
@@ -28,14 +31,22 @@ void dfs(int s)
     vis[s] = 1;
     pre[s] = cnt++;
     low[s] = pre[s];
-    for (int i = 0; i < N; ++i) if (!vis[i] && adj[s][i]) {  // tree edge
-        parent[i] = s;
-        dfs(i);
-        low[s] = min(low[s], low[i]);
+    for (int x = 0; x < nedge[s]; ++x) {
+        int i = adj[s][x];
+        if (!vis[i]) {  // tree edge
+            parent[i] = s;
+            dfs(i);
+            low[s] = min(low[s], low[i]);
+        } else {  // back edge
+            if (parent[s] != i) low[s] = min(low[s], pre[i]);
+        }
     }
-    for (int i = 0; i < N; ++i) if (adj[s][i] && parent[s] != i) {  // back edge
-        low[s] = min(low[s], pre[i]);
+    /*
+    for (int x = 0; x < nedge[s]; ++x) {
+        int i = adj[s][x];
+        if (parent[s] != i) low[s] = min(low[s], pre[i]);
     }
+    */
 }
 
 int count_ap(int root)
@@ -45,45 +56,50 @@ int count_ap(int root)
     parent[root] = -1;
     dfs(root);
 
-    /*
-    for (int i = 0; i < N; ++i) {
-        cout << i << ' ' << pre[i] << ' ' << low[i] << endl;
-    }
-    */
-
+    for (int i = 0; i < N; ++i) is_ap[i] = false;
     int ans = 0;
+    int nchild = 0;
     // for non-root, it is AP iff there is a child has low[j] >= pre[i]
-    for (int i = 0; i < N; ++i) if (i != root) {
-        bool is_ap = false;
-        for (int j = 0; j < N; ++j) if (parent[j] == i) {
-            if (low[j] >= pre[i]) is_ap = true;
-        }
-        ans += (is_ap);
+    for (int j = 0; j < N; ++j) {
+        int i = parent[j];
+        if (i == root) nchild++;
+        if (low[j] >= pre[i] && i != root) is_ap[i] = true;
     }
     // for root, it is AP iff it has at least 2 children
-    int nchild = 0;  // stupid, use bool for nchild
-    for (int j = 0; j < N; ++j) if (parent[j] == root) nchild++;
-    ans += (nchild > 1);
+    is_ap[root] = (nchild > 1);
+    for (int i = 0; i < N; ++i) ans += (is_ap[i]);
     return ans;
 }
 
 int main()
 {
-    int T; cin >> T;
+    int T; scanf("%d", &T); //cin >> T;
     while (T--) {
+        //cin >> N >> M >> K;
+        scanf("%d%d%d", &N, &M, &K);
         for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N; ++j)
-            adj[i][j] = 0;
+            nedge[i] = 0;
 
-        cin >> N >> M >> K;
         for (int i = 0; i < M; ++i) {
-            int a, b; cin >> a >> b;
-            adj[a][b] = adj[b][a] = 1;
+            int a, b; //cin >> a >> b;
+            scanf("%d%d", &a, &b);
+            adj[a][nedge[a]++] = b;
+            adj[b][nedge[b]++] = a;
         }
         int num_shield = count_ap(0);
         int cost = num_shield * K;
-        cout << cost << endl;
+        //cout << cost << endl;
+        printf("%d\n", cost);
     }
 }
 
+// Correct Answer
+// Execution Time: 3.20
+// must be cin/cout slow I/O, for N=3000 and M=N*(N-1)/2
+// the input size is 80MB
+// cin/cout runs in 0m2.386s
+// scanf/printf runs in 0m0.792s
+//
+// TLE
+// use adjlist to speed up dfs, still TLE
 // OK, got TLE now

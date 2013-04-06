d9f987f77377a282e374816f00d6afee56f1e381
lyan <lyan@lyan-ThinkPad-T430.(none)>
2013-04-05 18:19:01 -0700

    some ideas

--- codechef/apr13/CHEFGAME.cpp
+++ codechef/apr13/CHEFGAME.cpp
@@ -1,4 +1,16 @@
 // CHEFGAME.cpp
 //
-// 747474747 =  3^1 761^1 327409^1
+// MOD is 747474747 =  3^1 761^1 327409^1
+//
+// optimal configuration must be a spanning tree, as there is no cycle
+// the problem is to find a spanning tree with prod of edge weight maximized
+//
+// w(A,B) = sum of (A[i]-B[i])^2
+// dim D <= 5
+//
+// edge weight is implicit, computing all pairs is already N^2
+//
+// T = 6666
+// N = 6666
+// D = 5
 
---
+++ codechef/apr13/CYLINDER.cpp
@@ -0,0 +1,18 @@
+// start with W x H
+//
+// cut into two pieces, either both have W or both have H
+//
+// cut one piece into two identical circles, pi*R*R
+//
+// cut the other piece into rectangle width = 2*pi*R, height = A
+//
+// to maximize: VOL = pi*R*R*A
+
+// T = 4*10^5
+// W, H = 10^6 -1
+//
+// binary search for the cut
+//
+// after cut, left or right to make circle
+// circle bigger ...
+

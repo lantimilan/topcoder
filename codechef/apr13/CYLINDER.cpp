ee7ff669e0a0f1189c8d1397be937873f6ced248
lyan <lyan@lyan-ThinkPad-T430.(none)>
2013-04-05 18:33:21 -0700

    cylinder

--- codechef/apr13/CYLINDER.cpp
+++ codechef/apr13/CYLINDER.cpp
@@ -16,3 +16,30 @@
 // after cut, left or right to make circle
 // circle bigger ...
 
+// given A and R, can they fit into W x H paper?
+//
+// three pieces: two squares of R x R and one rectangle of A x R
+//
+// could be circle, circle, rect
+// or
+// circle rect
+// circle
+//
+// so the problem is to fit in two pieces, 2R x R and A x R
+// or R x 2R and A x R
+//
+// the goal is to maximize R*R*A
+//
+
+// option 1: 2R x R and A x R, and use W x H
+// 2R + A <= W
+// R + R  <= H
+//
+// to maximize R*R*A <= R*R*(W - 2R)
+// we know R <= H / 2.0 and R <= W / 2.0 since A >= 0
+// so R <= min(H, W) / 2.0
+//
+// In this case must have W > H
+// Take derivative, 2WR - 2*3R^2 = 0, 2W - 6R = 0, R = W / 3.0
+
+// Overall not too bad, just case analysis

83d9992ff3719df028748d4c27900e5ebe7e505f
Li Yan <lantimilan@gmail.com>
2013-05-11 13:01:46 -0700

    border rectangle O(n^3)

--- interviewstreet/hacker101/max_khash.cpp
+++ interviewstreet/hacker101/max_khash.cpp
@@ -3,6 +3,7 @@
 // binary search
 
 #include <cassert>
+#include <cstring>
 #include <iostream>
 using namespace std;
 
@@ -11,6 +12,8 @@ int m, n;
 char table[505][505];
 int row[505][505];
 int col[505][505];
+int strip[505][505];
+int hop[505][505];
 
 int main()
 {
@@ -19,13 +22,6 @@ int main()
         string s; cin >> s;
         for (int j = 0; j < n; ++j) table[i][j] = s[j];
     }
-
-//    for (int i = 0; i < m; ++i)
-//    {
-//    for (int j = 0; j < n; ++j)
-//        cout << table[i][j];
-//    cout << endl;
-//    }
 
     // row span from (i,j) to right
     for (int i = 0; i < m; ++i) {
@@ -36,26 +32,53 @@ int main()
     }
 
     for (int j = 0; j < n; ++j) {
-        col[n-1][j] = (table[n-1][j] == '.');
-        for (int i = n-2; i >= 0; --i)
+        col[m-1][j] = (table[m-1][j] == '.');
+        for (int i = m-2; i >= 0; --i)
             if (table[i][j] == 'x') col[i][j] = 0;
             else col[i][j] = col[i+1][j] + 1;
     }
 
     int best = 0;
-    for (int i = 0; i < m; ++i)
-    for (int j = 0; j < n; ++j)
-    for (int w = 2; w <= n; ++w)
     for (int h = 2; h <= m; ++h)
-    if (i+h-1 < m && j+w-1 < n)
     {
-        if (row[i][j] >= w && row[i+h-1][j] >= w
-            && col[i][j] >= h && col[i][j+w-1] >= h)
-            {
-            best = max(best, 2*((w-1) + (h-1)));
+        memset(strip, 0, sizeof strip);
+        memset(hop, 0, sizeof hop);
+        // hop[i][j][h] is dist to right most consecutive hop
+        for (int j = n-1; j >= 0; --j) {
+            for (int i = m-1; i >= 0; --i) {
+                if (i+h-1 < m) {
+                    if (table[i][j] == '.' && table[i+h-1][j] == '.') {
+                        hop[i][j] = 1;
+                        if (j+1 < n) hop[i][j] = hop[i][j+1] + 1;
+                    }
+                }
             }
+        }
+        // find right most strip within hop
+        for (int j = n-1; j >= 0; --j) {
+            for (int i = m-1; i >= 0; --i) if (hop[i][j] > 0) {
+                if (j+1 < n && strip[i][j+1] > 0) {
+                    strip[i][j] = strip[i][j+1] + 1;
+                } else {
+                    if (col[i][j] >= h) strip[i][j] = 1;
+                }
+            }
+        }
+
+        int wmax = 0;
+        for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+        if (col[i][j] >= h)
+        {
+            wmax = max(wmax, strip[i][j]);
+        }
+        //cout << h << ' ' << wmax << endl;
+        if (wmax > 1)
+        best = max(best, 2*(h-1 + wmax-1));
     }
 
     if (best == 0) cout << "impossible\n";
     else cout << best << endl;
 }
+
+// Accepted

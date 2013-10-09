e74fc963f3803a7a28b3b88ffaa855d204314048
Li Yan <lantimilan@gmail.com>
2013-10-08 21:19:27 -0700

    use s1, s2 as score

--- tc/SRM593/div2p1000.cpp
+++ tc/SRM593/div2p1000.cpp
@@ -42,9 +42,25 @@ using namespace std;
 //
 // we update best with max(s1, s2)
 
+// alternative DP
+// let dp[i][s] be true if A[0..i-1] can form a partition with score s,
+// that is, for the partition, max1 - min2 >= max2 - min1
+//
+// base cae dp[0][0] = true
+// after we computed dp[i][s] for all possible s for A[0..i-1]
+// for each valid s,
+// we could add A[i] to either team max or team min
+// for team max, t1 = s + B[i], and t1 is a valid score for A[0..i]
+// for team min, s1 = s, s2 = smax - smin - s1
+// t1 = s1 - A[i], t2 = s2 + B[i]
+// we have max(t1, t2) as a valid score for A[0..i]
+// in other words, we have just extended a configuration for A[0..i-1]
+// to A[0..i] by adding A[i]
+//
+// score can be as low as -smin, and as high as +smax
+
 const int D = 50*10000;
-//vector<vector<bool> > dp(51, vector<bool>(2*D+3));
-bool dp[2][4*D+3];
+bool dp[2][2*D+3];
 
 class MayTheBestPetWin
 {
@@ -52,41 +68,42 @@ public:
 
 int calc(vector <int> A, vector <int> B)
 {
-    int smin = 0, smax = 0;
+    int smin[55], smax[55];
+    smin[0] = smax[0] = 0;
     for (int i = 0; i < A.size(); ++i) {
-        smin += A[i];
-        smax += B[i];
+        smin[i+1] = smin[i] + A[i];
+        smax[i+1] = smax[i] + B[i];
     }
-    int cap = smin + smax;  // offset of (max1 - max2) + (min1 - min2)
-    int lim = 2*cap;  // s1 - s2 must be within [-cap, +cap]
-    dp[0][cap] = true;
+    int N = A.size();
+    int cap = smin[N] + smax[N];
+    int diff = smin[N];
+    dp[0][diff] = true;
     for (int i = 0; i < A.size(); ++i) {
         //cout << "iter " << i << endl;
         int p1 = i&1;
         int p2 = (i+1)&1; //cout << p1 << ' ' << p2 << endl;
         memset(dp[p2], 0, sizeof(dp[p2])); //cout << "memset done" << endl;
-        assert(lim <= 4*D);
-        for (int d = 0; d <= lim; ++d) if (dp[p1][d]) {
-            int d2;
-            // use A[i],B[i] in team1
-            d2 = d + B[i] + A[i]; if (0 <= d2 && d2 <= lim) { dp[p2][d2] = true; }
-            // use A[i],B[i] in team2
-            d2 = d - (B[i] + A[i]); if (0 <= d2 && d2 <= lim) { dp[p2][d2] = true; }
+        for (int d = 0; d <= cap; ++d) if (dp[p1][d]) {
+            int s1, s2, t1, t2;
+            s1 = d - diff; s2 = smax[i] - smin[i] - s1;
+            // use A[i], B[i] in team max
+            t1 = s1 + B[i]; t2 = s2 - A[i];
+            t1 = (abs(t1) >= abs(t2)) ? t1 : t2;
+            t1 += diff;
+            assert(0 <= t1 && t1 <= cap);
+            dp[p2][t1] = true;
+            // use A[i],B[i] in team min
+            t1 = s1 - A[i]; t2 = s2 + B[i];
+            t1 = (abs(t1) >= abs(t2)) ? t1 : t2;
+            t1 += diff;
+            assert(0 <= t1 && t1 <= cap);
+            dp[p2][t1] = true;
         }
     }
-    int alldiff = 0;
-    for (int i = 0; i < A.size(); ++i) {
-        alldiff += B[i] - A[i];
-    }
-    cout << "dp done\n";
     int best = 2*D;
-    for (int d = 0; d <= lim; ++d) {
+    for (int d = 0; d <= cap; ++d) {
         if (dp[A.size()&1][d]) {
-            int s1, s2;
-            int s = d - cap;
-            s1 = (alldiff + s)/2;
-            s2 = s1 - s;
-            best = min(best, max(s1, s2));
+            best = min(best, abs(d - diff));
         }
     }
     return best;

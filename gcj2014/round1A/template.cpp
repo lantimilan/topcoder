c9452d069d0eb7f5a70d278c3ed0e936797fe5b7
Li Yan <lantimilan@gmail.com>
2014-04-25 20:47:39 -0700

    round 1A

---
+++ gcj2014/round1A/template.cpp
@@ -0,0 +1,27 @@
+// template.cpp
+//
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve(int tcase)
+{
+    cout << "Case #" << tcase << ": ";
+}
+
+int main()
+{
+    int T; cin >> T;
+    for (int t = 1; t <= T; ++t)
+        solve(t);
+}

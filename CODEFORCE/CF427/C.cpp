2891e24c375f1c4f22a7c0cad2afce62d60e6c12
Li Yan <lantimilan@gmail.com>
2014-05-14 23:40:38 -0700

    SCC in linear time

--- CODEFORCE/CF427/C.cpp
+++ CODEFORCE/CF427/C.cpp
@@ -2,8 +2,16 @@
 //
 // linear time SCC
 // ans = \Prod {# of min price vertex in that component}
+//
+// SCC using two dfs
+// 1. dfs on G to get finish time of each node
+// 2. compute Grev
+// 3. dfs on Grev, process node in the reverse order of finish time
+// that is, last finished node processed first
+// 4. each dfs tree in step 3 is a component
 
 #include <algorithm>
+#include <cassert>
 #include <cstring>
 #include <iostream>
 #include <map>
@@ -12,35 +20,41 @@ using namespace std;
 
 const int MX = 1e5 + 5;
 int cost[MX];
-int dfspre[MX];
-int dfspost[MX];
 int comp[MX];
-int gclock;
-pair<int,int> nodepair[MX];
+int vis[MX];
+int minnode;
+int mincost;
+vector<int> dfslist;
+
 
 vector<vector<int> > graph;
 vector<vector<int> > rev_graph;
-map<int, pair<int, int> > comp_map;  // compid, (cost, cnt)
+map<int, pair<int, int> > comp_map;  // comp, (mincost, numnode)
 
 void dfs(int start)
 {
-    dfspre[start] = gclock++;
+    vis[start] = 1;
     for (int x = 0; x < graph[start].size(); ++x) {
         int next = graph[start][x];
-        if (dfspre[next] < 0)
-            dfs(next);
+        if (!vis[next]) dfs(next);
     }
-    dfspost[start] = gclock++;
+    dfslist.push_back(start);
 }
 
 void dfs_mark(int start, int id)
 {
     comp[start] = id;
+    /*
+    if (cost[start] < mincost) {
+        mincost = cost[start];
+        minnode = 1;
+    } else if (cost[start] == mincost) {
+        minnode++;
+    }
+    */
     for (int x = 0; x < rev_graph[start].size(); ++x) {
         int next = rev_graph[start][x];
-        if (dfspre[id] < dfspre[next] && dfspost[id] > dfspost[next])
-        if (comp[next] < 0)
-            dfs_mark(next, id);
+        if (comp[next] < 0) dfs_mark(next, id);
     }
 }
 
@@ -57,48 +71,45 @@ int main()
         graph[u].push_back(w);
         rev_graph[w].push_back(u);
     }
-    memset(dfspre, -1, sizeof dfspre);
-    gclock = 0;
-    for (int i = 0; i < n; ++i) if (dfspre[i] < 0)
-        dfs(i);
-
-//    for (int i = 0; i < n; ++i)
-//        cout << i << ' ' << dfspre[i] << endl;
-
-    for (int i = 0; i < n; ++i)
-        nodepair[i] = make_pair(dfspre[i], i);
-    sort(nodepair, nodepair+n);
-
-    memset(comp, -1, sizeof comp);
-    for (int x = 0; x < n; ++x) {
-        int id = nodepair[x].second;
-        if (comp[id] >= 0) continue;
-        dfs_mark(id, id);
-    }
-//    for (int i = 0; i < n; ++i)
-//        cout << i << ' ' << comp[i] << endl;
-
-    for (int i = 0; i < n; ++i) {
-        int cc = comp[i];
-        if (comp_map.count(cc)) {
-            int mincost = comp_map[cc].first;
-            if (cost[i] < mincost)
-                comp_map[cc] = make_pair(cost[i], 1);
-            else if (cost[i] == mincost)
-                comp_map[cc].second++;
-        } else {
-            comp_map[cc] = make_pair(cost[i], 1);
-        }
-    }
-
     const int MOD = 1e9+7;
     long long money = 0;
     int ans = 1;
-    map<int, pair<int, int> >::iterator it;
+    memset(vis, 0, sizeof vis);
+    memset(comp, -1, sizeof comp);
+
+    // dfs on G to compute finish time
+    // nodes appended to dfslist in order of finish time
+    for (int i = 0; i < n; ++i) if (!vis[i]) {
+        dfs(i);
+    }
+    assert(dfslist.size() == n);
+
+    // dfs on Grev, every dfs tree is a component
+    for (int i = n-1; i >= 0; --i) {
+        int node = dfslist[i];
+        if (comp[node] < 0) dfs_mark(node, node);
+    }
+    comp_map.clear();
+    for (int i = 0; i < n; ++i) {
+        int c = comp[i];
+        if (comp_map.count(c)) {
+            int mincost = comp_map[c].first;
+            int numnode = comp_map[c].second;
+            if (cost[i] < mincost) {
+                comp_map[c] = make_pair(cost[i], 1);
+            } else if (cost[i] == mincost) {
+                comp_map[c].second++;
+            }
+        } else {
+            comp_map[c] = make_pair(cost[i], 1);
+        }
+    }
+    map<int, pair<int,int> >::iterator it;
     for (it = comp_map.begin(); it != comp_map.end(); ++it) {
-        money += it->second.first;
-        int cnt = it->second.second;
-        ans = (long long) ans * cnt % MOD;
+        int mincost = it->second.first;
+        int numnode = it->second.second;
+        ans = (long long)ans * numnode % MOD;
+        money += mincost;
     }
     cout << money << ' ' << ans << endl;
 }

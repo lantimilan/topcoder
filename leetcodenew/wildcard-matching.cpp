62d0f8b2e42faa4ebf31b9337f260221a5a43b39
Li Yan <lantimilan@gmail.com>
2014-06-14 16:18:28 -0700

    still TLE

--- leetcodenew/wildcard-matching.cpp
+++ leetcodenew/wildcard-matching.cpp
@@ -1,3 +1,9 @@
+// wildcard-matching.cpp
+//
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     bool isMatch(const char *src, const char *patt) {
@@ -30,14 +36,17 @@ public:
             if (!charMatch(*s, *p)) return false;
         }
         send = s+1; pend = p;  // now we work on src[0..send-1] and patt[0..pend-1], we know patt[pend] is '*'
+        cout << "text end " << send - src << ' ' << "patt end" << pend - patt << endl;
         start = src;
         for (p = patt; *p; ) {
             // find the first segment start at nonstar and end with a '*'
             for (; *p && *p == '*'; ++p) ;
             if (!*p) return true;  // all nonstar in patt matched
             for (q = p+1; *q && *q != '*'; ++q) ;
+            cout << "p q " << (p-patt) << ' ' << (q-patt) << endl;
             // now match patt[p..q-1] to src[start..send]
             const char *next = KMPmatch(start, send, p, q);
+            cout << "next " << next - src << endl;
             if (next == NULL) return false;  // cannot find patt[p..q-1] in text[start..send-1]
             else start = next + (q-p);
             p = q+1;
@@ -47,14 +56,18 @@ public:
 
     void KMPcalc(const char *start, const char *end, vector<int> &q)
     {
-        int m = end - start;
+        cout << "KMPcalc " << start << ' ' << end << endl;
+        int m = end - start; cout << "KMPcalc m = " << m << endl;
         q.resize(m); q[0] = 0;
         for (int i = 1; i < m; ++i) {
+            cout << "i = " << i << endl;
             int k = i-1;
-            while (k > 0 && !charMatch(start[i], start[q[k]])) k = q[k];
-            if (start[i] == start[q[k]]) q[i] = q[k] + 1;
-            else q[k] = 0;
+            while (k > 0 && !charMatch(start[i], start[q[k]])) k = q[k]-1;  // this is critical, we know suffix end at k has prefix length q[k]
+            // hence next pos to check is q[k]-1, which is of length q[k] for sub[0..q[k]-1]
+            if (charMatch(start[i], start[q[k]])) q[i] = q[k] + 1;
+            else q[i] = 0;
         }
+        cout << "KMPcalc  done" << start << ' ' << end << endl;
     }
     const char *KMPmatch(const char *textStart, const char *textEnd, const char *pattStart, const char *pattEnd)
     {
@@ -64,6 +77,9 @@ public:
         n = textEnd - textStart; m = pattEnd - pattStart;
         vector<int> q;
         KMPcalc(pattStart, pattEnd, q);
+
+        cout << pattStart << ' ' << pattEnd << endl;
+        for (int i = 0; i < q.size(); ++i) cout << q[i] << ' '; cout << endl;
 
         int i, j;
         for (i = j = 0; i < n; ) {
@@ -86,5 +102,13 @@ public:
     }
 };
 
-// TLE on
-//      "aabcdedefghijkl", "aabc*"
+int main()
+{
+    Solution sol;
+    const char *text = "aabcdedefghijkl";
+    const char *patt = "aabc*";
+    bool good = sol.isMatch(text, patt);
+    cout << text << ' ' << patt << ' ' << good << endl;
+}
+// TLE on "aaabbaabbaab", "*aabbaa*a*"
+// TLE on (cleared, KMPcalc needs to move to q[k]-1 for next pos

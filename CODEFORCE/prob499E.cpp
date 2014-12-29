dc11fca2567aa2c2e1b6905d4db5009ec68f38d4
Li Yan <lantimilan@gmail.com>
2014-12-28 20:13:05 -0800

    maximum matching of problem E

--- CODEFORCE/prob499E.cpp
+++ CODEFORCE/prob499E.cpp
@@ -1,3 +1,14 @@
+// prob499E.cpp
+// maximum matching
+// The key is: good pairs (i,j) has i+j being odd
+// build graph
+// vertex, for each elemnt a[i] and its prime factor p,
+// make a vertex v = (a[i], p)
+// for each pair (i,j) and some prime p
+// connect (a[i],p) with (a[j],p), this means we divide a[i] and a[j] by p
+// if p occurs r times, add r vertices of the form (a[i], p)
+// then it is a maximum matching of bipartite graph
+
 #include <iostream>
 using namespace std;
 

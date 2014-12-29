ef835befd28c943b1371e1274ae8af20dc8f1bb8
Li Yan <lantimilan@gmail.com>
2014-12-28 20:28:55 -0800

    problem D

--- CODEFORCE/prob499D.cpp
+++ CODEFORCE/prob499D.cpp
@@ -1,3 +1,12 @@
+// prob499D.cpp
+// state: dp[i][j] expected number of guessed songs for the first i songs
+// and just before j-th second
+// dp[i][j] can move into dp[i+1][j+1] ... dp[i+1][j+t[i]-1]
+//
+// the part I was confused is to realize the Bernoulli process
+// keep trying until succeed, with p = p[i]
+// and stop at t[i] seconds
+
 #include <iostream>
 #include <iomanip>
 using namespace std;

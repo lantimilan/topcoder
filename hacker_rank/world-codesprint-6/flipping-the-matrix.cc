e680796024ab3697caca70913866f49730b08d34
Li Yan <lantimilan@gmail.com>
2016-08-29 16:10:27 -0700

    edit comment

--- hacker_rank/world-codesprint-6/flipping-the-matrix.cc
+++ hacker_rank/world-codesprint-6/flipping-the-matrix.cc
@@ -1,4 +1,12 @@
 // flipping-the-matrix.cc
+// this is an interesting problem because it is not obvious at first what
+// can be done. The operations are dependent and order does matter. So
+// we should think more on how to get max and turns out each cell in the
+// upperleft can get value from one of the four choices and you can have
+// all nxn submatrix taking max for every cell. We do it row by row and
+// within each row, one by one, using at most 3 reverse ops without
+// touching any of the k-1 elements already in place when moving kth
+// element.
 #include <iostream>
 using namespace std;
 
